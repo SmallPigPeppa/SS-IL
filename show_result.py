56be5e3b10b459abf4cd9058f1531dc52effd284
SmallPigPeppa <42854223+SmallPigPeppa@users.noreply.github.com>
2022-08-30 15:40:32 +0800

    intial resnet50

--- show_result.py
+++ show_result.py
@@ -8,9 +8,12 @@ for i, filename in enumerate(os.listdir(result_dir)):
     if ext == '.pkl':
         all_pkl.append(filename)
         print(f"{i}:{basename}")
-pkl_idx = input("Input pkl idx:")
 
-pkl_path = os.path.join(result_dir, all_pkl[int(pkl_idx)])
-file = open(pkl_path, "rb")
-data = pickle.load(file)
-print(data)
+while True:
+    pkl_idx = int(input("Input pkl idx:\n input -1 to break up"))
+    if pkl_idx ==-1:
+        break
+    pkl_path = os.path.join(result_dir, all_pkl[pkl_idx])
+    file = open(pkl_path, "rb")
+    data = pickle.load(file)
+    print(data)
